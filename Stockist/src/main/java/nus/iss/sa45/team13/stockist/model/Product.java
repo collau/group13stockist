2e99cf4dc7b6e9bb5a8424b19dd10fde0c737326
eugene <eugene@172.17.254.95>
2017-12-09 13:16:49 +0800

    new product class

---
+++ Stockist/src/main/java/nus/iss/sa45/team13/stockist/model/Product.java
@@ -0,0 +1,11 @@
+package nus.iss.sa45.team13.stockist.model;
+
+public class Product {
+	
+	private int partnumber;
+	private String partName;
+	private String description;
+	private String color;
+	private String dimension;
+
+}
