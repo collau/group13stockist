542e060c2c161738e3ddadac0b4f7399e1cbf7ac
Xavier V <Xavier V@DESKTOP-V757L6Q>
2017-12-13 22:36:06 +0800

    Removing unwanted Databases. Only the latest database is left remaining.

--- Stockist/SQL/insertStaffDetails.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `sa45grp13ca`.`staffdetails` (`staffid`, `name`, `role`) VALUES ('1', 'ThetThet', 'Admin');
-INSERT INTO `sa45grp13ca`.`staffdetails` (`staffid`, `name`, `role`) VALUES ('2', 'Danny Hardy', 'Staff');
-INSERT INTO `sa45grp13ca`.`staffdetails` (`staffid`, `name`, `role`) VALUES ('3', 'Nicklos', 'Staff');
-INSERT INTO `sa45grp13ca`.`staffdetails` (`staffid`, `name`, `role`) VALUES ('4', 'Emiley Cooper', 'Staff');
-INSERT INTO `sa45grp13ca`.`staffdetails` (`staffid`, `name`, `role`) VALUES ('5', 'Megan Pole', 'Staff');
-INSERT INTO `sa45grp13ca`.`staffdetails` (`staffid`, `name`, `role`) VALUES ('6', 'Billy Robinson', 'Staff');
-INSERT INTO `sa45grp13ca`.`staffdetails` (`staffid`, `name`, `role`) VALUES ('7', 'Zara Cole', 'Staff');
-INSERT INTO `sa45grp13ca`.`staffdetails` (`staffid`, `name`, `role`) VALUES ('8', 'Sam Smith', 'Admin');
-INSERT INTO `sa45grp13ca`.`staffdetails` (`staffid`, `name`, `role`) VALUES ('9', 'Shawn Mendes', 'Admin');
